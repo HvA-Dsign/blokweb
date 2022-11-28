2cd8cb7f6f85f136d9d63f4745ee892d4a10d610
HvA-Dsign <118122098+HvA-Dsign@users.noreply.github.com>
2022-11-28 21:36:55 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,26 @@
-// JavaScript Document
 console.log("hi");
+
+
+var rodeKnoppen = document.querySelectorAll("article button");
+
+rodeKnoppen.forEach(knop => {
+    console.log(knop);
+
+    knop.addEventListener("click", maakKnopZwart);
+
+});
+
+
+function maakKnopZwart(event) {
+    // aanspreken van de button door event te gebruiken
+    event.target.classList.toggle('anders');
+    // kijken of de class anders bevat
+    if (event.target.classList.contains('anders')) {
+        event.target.innerHTML = "Bewaard";
+    }
+    // niet anders
+    else {
+        event.target.innerHTML = "Bewaren";
+        console.log(event);
+    }
+}
